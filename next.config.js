5bc92f53d8c58da730597b3d3f6f0382e2da94b4
sophievet07 <sophievet07@gmail.com>
2026-08-04 12:36:38 +0530

    next.config.js

--- next.config.js
+++ next.config.js
@@ -1,9 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  reactStrictMode: true,
-  env: {
-    NEXT_PUBLIC_SUPABASE_URL: process.env.NEXT_PUBLIC_SUPABASE_URL,
-    NEXT_PUBLIC_SUPABASE_ANON_KEY: process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY,
+  output: 'export',
+  trailingSlash: true,
+  images: {
+    unoptimized: true
   }
 }
+
 module.exports = nextConfig
